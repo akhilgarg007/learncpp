ba44a144a65aa667728d03d3f338c9b06bea94ae
akhilgarg007 <18476911+akhilgarg007@users.noreply.github.com>
2019-08-20 12:17:26 +0530

    2_x_question_1.cpp

---
+++ 2_x_question_1.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <vector>
+#include <utility>
+using namespace std;
+
+int readnumber() {
+	int input;
+	cout << "Enter Number: ";
+	cin >> input;
+	return input;
+}
+
+void writeAnswer(int x) {
+	cout << "The sum of the numbers is: " << x << endl;
+}
+
+
+int main() {
+	//readnumber
+	int input1 = readnumber();
+	//readnumber
+	int input2 = readnumber();
+	//add numbers
+	int output = input1 + input2;
+	//write answer
+	writeAnswer(output);
+}
